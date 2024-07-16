e5d3bb6a1195498fea7320caa7df3b25c3b280c4
tifirstmedperu <tifirstmed@gmail.com>
2024-07-16 18:15:08 -0500

    Update create-lead.ts

--- app/api/create-lead.ts
+++ app/api/create-lead.ts
@@ -10,31 +10,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const {  name, lastName, phone, email } = req.body;
     
-    const webhookUrl = 'https://firstmedpharma.bitrix24.es/rest/752775/6m6diw0wk65vzdxz'; // Reemplaza con tu URL de webhook
-
-    /* 
-    values: {"LEAD_NAME":["Prueba Test"],"LEAD_LAST_NAME":["Test Prueba Demo"],"LEAD_PHONE":[" 51 (994) 936-990"],"LEAD_EMAIL":["cesar@gmail.com"],"LEAD_UF_CRM_LEAD_1686584348364":["Test prueba"]}
-
+    const webhookUrl = 'https://firstmedpharma.bitrix24.es/rest/752775/y1krya81wzz6tdz1'; //
     
-    */
-
     const leadData = {
         fields: {
             TITLE: "Formulario Prueba",
-            LEAD_NAME: "Cesar Test",
-            LEAD_LAST_NAME: "Data Test",
-            LEAD_WEB:"Landing Form",
-            LEAD_STATUS_ID: "NEW",
-            LEAD_PHONE: [
-                { "VALUE": " 51 (987) 654-123", "VALUE_TYPE": "MOBILE" }
+            NAME: "Cesar Test",
+            LAST_NAME: "Data Test",
+            WEB: "Landing Form",
+            STATUS_ID: "NEW",
+            PHONE: [
+                { "VALUE": "+51 (987) 654-123", "VALUE_TYPE": "WORK" }
             ],
-            LEAD_EMAIL: [
+            EMAIL: [
                 { "VALUE": "ccesar@gmail.com", "VALUE_TYPE": "HOME" }
             ],
-            LEAD_UF_CRM_LEAD_1686584348364:"Test demo"
+            UF_CRM_LEAD_1686584348364: "Test demo"
+        },
+        consents: {
+            "AGREEMENT_25": "Y"
         }
     };
-    console.log(leadData);
 
     try {
         const response = await axios.post(`${webhookUrl}/crm.lead.add.json`, leadData);
