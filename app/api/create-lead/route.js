559cedfa3b6adfe5a65712c2b17a9f3ec089f1fc
@WebMaster-Firstmed <tifirstmed@gmail.com>
2024-07-16 21:58:12 -0500

    fix: integration-CRM

---
+++ app/api/create-lead/route.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+
+export async function POST(req, res) {
+  try {
+    const body = await req.json(); // Obtener los datos del cuerpo de la solicitud
+    const { name, lastName, phone, email, description } = body;
+
+    const webhookUrl =
+      'https://firstmedpharma.bitrix24.es/rest/752775/y1krya81wzz6tdz1'; // Reemplaza con tu URL de webhook
+
+    const leadData = {
+      fields: {
+        TITLE: 'Formulario Prueba',
+        STATUS_ID: 'UC_LROQI4',
+        WEB: 'Landing Form',
+        NAME: name,
+        LAST_NAME: lastName,
+        PHONE: [{ VALUE: phone, VALUE_TYPE: 'WORK' }],
+        EMAIL: [{ VALUE: email, VALUE_TYPE: 'HOME' }],
+        UF_CRM_LEAD_1686584348364: description,
+      },
+      params: {
+        REGISTER_SONET_EVENT: 'Y',
+      },
+    };
+
+    const response = await axios.post(
+      `${webhookUrl}/crm.lead.add.json`,
+      leadData
+    );
+
+    return NextResponse.json(response.data);
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      // Manejar errores de Axios
+      const axiosError = err;
+      console.error(
+        'Error al crear el lead en Bitrix24:',
+        axiosError.response?.data || axiosError.message
+      );
+      return NextResponse.json(
+        {
+          error: 'Failed to create lead in Bitrix24',
+          details: axiosError.response?.data || axiosError.message,
+        },
+        { status: 500 }
+      );
+    } else {
+      // Manejar otros tipos de errores
+      console.error('Unexpected error:', err);
+      return NextResponse.json(
+        { error: 'An unexpected error occurred', details: err },
+        { status: 500 }
+      );
+    }
+  }
+}
--- app/components/Form/RegisterForm.tsx
+++ app/components/Form/RegisterForm.tsx
@@ -1,66 +1,75 @@
-"use client";
+'use client';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { userSchema } from './userScheema';
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 import { initFacebookPixel } from '@/app/utils/facebookPixel';
 import { trackFacebookEvent } from '@/app/utils/facebookPixel';
 
 type User = {
   name: string;
   lastname: string;
-  phoneNumber: number;
+  phoneNumber: string;
   email: string;
   description: string;
   acceptTerms: boolean;
 };
 
-declare const fbq: Function;
+let isFacebookPixelInitialized = false;
 
 const RegisterForm: React.FC = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<User>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>({
     resolver: zodResolver(userSchema),
   });
 
   const onSubmit = async (data: User) => {
-    
     const formattedData = {
-      ...data,
-      phoneNumber: `+51${data.phoneNumber}`,
+      name: data.name,
+      lastName: data.lastname,
+      phone: `+51 ${data.phoneNumber}`, // Asegurarse de que el número de teléfono esté correctamente formateado
+      email: data.email,
+      description: data.description,
     };
     trackFacebookEvent('Lead', formattedData);
     try {
       const response = await fetch('/api/create-lead', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formattedData),
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formattedData),
       });
 
       if (response.status === 200) {
-        console.log("Datos enviados correctamente a Bitrix24");
+        console.log('Datos enviados correctamente a Bitrix24');
         // Aquí podrías redirigir o mostrar un mensaje de éxito
-    } else {
-        console.log("Error al enviar los datos a Bitrix24");
+      } else {
+        console.log('Error al enviar los datos a Bitrix24');
         // Manejar el caso de error según sea necesario
-    }
-  } catch (error) {
+      }
+    } catch (error) {
       console.log(error);
-  }
-
+    }
   };
 
   const router = useRouter();
 
   useEffect(() => {
-    // Inicializa el píxel de Facebook al cargar la aplicación
-    initFacebookPixel();
+    if (!isFacebookPixelInitialized) {
+      // Inicializa el píxel de Facebook solo una vez
+      initFacebookPixel();
+      isFacebookPixelInitialized = true;
 
-    trackFacebookEvent('ViewContent');
+      // Envía el evento de vista de contenido
+      trackFacebookEvent('ViewContent');
+    }
 
-    // Agrega un listener al evento de cambio de ruta usando router.push
+    // Función para suscribirse a cambios de ruta
     const subscribeToRouteChanges = () => {
       router.push('/');
     };
@@ -75,40 +84,94 @@ const RegisterForm: React.FC = () => {
   }, [router]);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='border-2 rounded-2xl border-green-500 max-w-[650px] w-full p-2 md:p-5 space-y-2 bg-green-100'>
-      <div className='flex flex-col'>
-        <label className='font-bold' htmlFor="name">Nombres:</label>
-        <input className='bg-green-300/60 px-2 rounded-md focus:outline-green-700' id="name" {...register('name')} />
-        {errors.name && <span className='text-red-500'>{errors.name.message}</span>}
-      </div>
-      <div className='flex flex-col'>
-        <label className='font-bold' htmlFor="lastname">Apelllidos:</label>
-        <input className='bg-green-300/60 px-2 rounded-md focus:outline-green-700' id="lastname" {...register('lastname')} />
-        {errors.lastname && <span className='text-red-500'>{errors.lastname.message}</span>}
-      </div>
-      <div className='flex flex-col'>
-        <label className='font-bold' htmlFor="phoneNumber">Celular:</label>
-        <input className='bg-green-300/60 px-2 rounded-md focus:outline-green-700' id="phoneNumber" {...register('phoneNumber')} />
-        {errors.phoneNumber && <span className='text-red-500'>{errors.phoneNumber.message}</span>}
-      </div>
-      <div className='flex flex-col'>
-        <label className='font-bold' htmlFor="email">Correo electrónico:</label>
-        <input className='bg-green-300/60 px-2 rounded-md focus:outline-green-700' id="email" {...register('email')} />
-        {errors.email && <span className='text-red-500'>{errors.email.message}</span>}
-      </div>
-      <div className='flex flex-col'>
-        <label className='font-bold' htmlFor="description">¿Qué desea tratar?:</label>
-        <input className='bg-green-300/60 px-2 rounded-md focus:outline-green-700' id="description" {...register('description')} />
-        {errors.description && <span className='text-red-500'>{errors.description.message}</span>}
-      </div>
-      <div className='flex flex-col'>
-        <label className='text-sm' htmlFor="acceptTerms">
-          <input id="acceptTerms" type="checkbox" {...register('acceptTerms')} />
-          {"  "}Al enviar, acepto las políticas de Privacidad
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="border-2 rounded-2xl border-green-500 max-w-[650px] w-full p-2 md:p-5 space-y-2 bg-green-100"
+    >
+      <div className="flex flex-col">
+        <label className="font-bold" htmlFor="name">
+          Nombres:
         </label>
-        {errors.acceptTerms && <span className='text-red-500'>{errors.acceptTerms.message}</span>}
+        <input
+          className="bg-green-300/60 px-2 rounded-md focus:outline-green-700"
+          id="name"
+          {...register('name')}
+        />
+        {errors.name && (
+          <span className="text-red-500">{errors.name.message}</span>
+        )}
       </div>
-      <button className='w-full h-10 bg-green-600 text-white font-bold' type="submit">Enviar</button>
+      <div className="flex flex-col">
+        <label className="font-bold" htmlFor="lastname">
+          Apelllidos:
+        </label>
+        <input
+          className="bg-green-300/60 px-2 rounded-md focus:outline-green-700"
+          id="lastname"
+          {...register('lastname')}
+        />
+        {errors.lastname && (
+          <span className="text-red-500">{errors.lastname.message}</span>
+        )}
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold" htmlFor="phoneNumber">
+          Celular:
+        </label>
+        <input
+          className="bg-green-300/60 px-2 rounded-md focus:outline-green-700"
+          id="phoneNumber"
+          {...register('phoneNumber')}
+        />
+        {errors.phoneNumber && (
+          <span className="text-red-500">{errors.phoneNumber.message}</span>
+        )}
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold" htmlFor="email">
+          Correo electrónico:
+        </label>
+        <input
+          className="bg-green-300/60 px-2 rounded-md focus:outline-green-700"
+          id="email"
+          {...register('email')}
+        />
+        {errors.email && (
+          <span className="text-red-500">{errors.email.message}</span>
+        )}
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold" htmlFor="description">
+          ¿Qué desea tratar?:
+        </label>
+        <input
+          className="bg-green-300/60 px-2 rounded-md focus:outline-green-700"
+          id="description"
+          {...register('description')}
+        />
+        {errors.description && (
+          <span className="text-red-500">{errors.description.message}</span>
+        )}
+      </div>
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="acceptTerms">
+          <input
+            id="acceptTerms"
+            type="checkbox"
+            {...register('acceptTerms')}
+          />
+          {'  '}Al enviar, acepto las políticas de Privacidad
+        </label>
+        {errors.acceptTerms && (
+          <span className="text-red-500">{errors.acceptTerms.message}</span>
+        )}
+      </div>
+      <button
+        className="w-full h-10 bg-green-600 text-white font-bold"
+        type="submit"
+      >
+        Enviar
+      </button>
     </form>
   );
 };
