85a66f1c0b9d8dfcf74e01b7b1019c39ed2243a5
@WebMaster-Firstmed <tifirstmed@gmail.com>
2024-07-18 22:24:01 -0500

    Update route.js

--- app/api/create-lead/route.js
+++ app/api/create-lead/route.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { NextResponse } from "next/server";
+import axios from 'axios';
+import { NextResponse } from 'next/server';
 
 export async function POST(req, res) {
   try {
@@ -7,22 +7,22 @@ export async function POST(req, res) {
     const { name, lastName, phone, email, description } = body;
 
     const webhookUrl =
-      "https://firstmedpharma.bitrix24.es/rest/752775/y1krya81wzz6tdz1"; // Reemplaza con tu URL de webhook
+      'https://firstmedpharma.bitrix24.es/rest/752775/y1krya81wzz6tdz1'; // Reemplaza con tu URL de webhook
 
     const leadData = {
       fields: {
-        TITLE: "Formulario Prueba",
-        STATUS_ID: "UC_LROQI4",
-        WEB: "Landing Form",
+        TITLE: 'Formulario Landing Dormilon-M',
+        STATUS_ID: 'UC_LROQI4',
+        WEB: 'Landing Form',
         NAME: name,
         LAST_NAME: lastName,
-        PHONE: [{ VALUE: phone, VALUE_TYPE: "WORK" }],
-        EMAIL: [{ VALUE: email, VALUE_TYPE: "HOME" }],
+        PHONE: [{ VALUE: phone, VALUE_TYPE: 'WORK' }],
+        EMAIL: [{ VALUE: email, VALUE_TYPE: 'HOME' }],
         UF_CRM_LEAD_1686584348364: description,
-        UF_CRM_1633353212: "Dormilon-M",
+        UF_CRM_1633353212: '677',
       },
       params: {
-        REGISTER_SONET_EVENT: "Y",
+        REGISTER_SONET_EVENT: 'Y',
       },
     };
 
@@ -37,21 +37,21 @@ export async function POST(req, res) {
       // Manejar errores de Axios
       const axiosError = err;
       console.error(
-        "Error al crear el lead en Bitrix24:",
+        'Error al crear el lead en Bitrix24:',
         axiosError.response?.data || axiosError.message
       );
       return NextResponse.json(
         {
-          error: "Failed to create lead in Bitrix24",
+          error: 'Failed to create lead in Bitrix24',
           details: axiosError.response?.data || axiosError.message,
         },
         { status: 500 }
       );
     } else {
       // Manejar otros tipos de errores
-      console.error("Unexpected error:", err);
+      console.error('Unexpected error:', err);
       return NextResponse.json(
-        { error: "An unexpected error occurred", details: err },
+        { error: 'An unexpected error occurred', details: err },
         { status: 500 }
       );
     }
